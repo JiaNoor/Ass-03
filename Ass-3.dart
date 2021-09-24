1f68e7f18ac8c53a0fb57a55598afeeba3840011
JiaNoor <jianoor053@gmail.com>
2021-09-24 17:11:34 +0500

    last commit

--- Ass-3.dart
+++ Ass-3.dart
@@ -28,4 +28,61 @@ void main(){
     checkPrime= '$num is prime number';
   }
   print(checkPrime);   
+
+//Q5
+  var num = 7;
+  for(var i = 1; i<=15 ; i++){
+    print("$num x $i = ${num*i}");
+  }  
+
+//Q6  
+  var fruits = ["apple", "banana", "mango", "orange" , "strawberry"];
+  for (var i in fruits){
+   print(i);
+ }  
+
+//Q7  
+for(var i = 1; i <= 100; i++){
+   if(i%5 == 0){
+     print(i);
+   }
+}
+
+//Q8  
+  double cel = 30;
+  double fahr = (cel * 9/5) + 32;
+  print("${cel}°C is ${fahr.toStringAsFixed(2)}°F");
+  fahr = 101;
+  cel = (fahr - 32) * 5/9 ;
+  print("${fahr}°F is ${cel.toStringAsFixed(2)}°C");
+
+//Q9  
+  var operand1 = 9;
+  var operand2 = 4;
+  var operator = '/';
+  if(operator == '+'){
+    print(operand1 + operand2);
+  }
+  else if(operator == '-'){
+    print(operand1 - operand2);
+  }
+  else if(operator == '*'){
+    print(operand1 * operand2);
+  }
+  else if(operator == '/'){
+    print(operand1 / operand2);
+  }
+  else if(operator == '%'){
+    print(operand1 % operand2);
+  }  
+
+//Q10  
+  var checkVowel = 'U';
+  var vowels = ['a','e','i','o','u','A','E','I','O','U'];
+  if(vowels.contains(checkVowel) == true ){
+    print("It is a VOWEL");
+  }
+  else{
+    print("It is a consonant");
+  }  
 }
